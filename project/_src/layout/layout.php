02573504d620f81b54fd7741045cd548fa7d367c
david <d.marsalone@gmail.com>
2019-02-15 06:43:55 +0100

    Update layout.php
    meta ogImage url absolue

--- project/_src/layout/layout.php
+++ project/_src/layout/layout.php
@@ -16,7 +16,7 @@ if(is_a($vv,"\Classiq\Models\Page")){
     the()->htmlLayout()->meta->title=$vv->urlpage->box()->meta_title_lang;
     the()->htmlLayout()->meta->description=$vv->urlpage->box()->meta_description_lang;
     if($vv->thumbnail){
-        the()->htmlLayout()->ogImage=$vv->thumbnail()->sizeCover(500,400)->bgColor("ffffff")->jpg()->href();
+        the()->htmlLayout()->ogImage=$vv->thumbnail()->sizeCover(500,400)->bgColor("ffffff")->jpg()->href(true);
         the()->htmlLayout()->ogImageWidth=500;
         the()->htmlLayout()->ogImageHeight=400;
     }
