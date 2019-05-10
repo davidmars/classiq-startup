d540c876dfec381f29675dbeab94b44985ee0482
david <d.marsalone@gmail.com>
2019-05-10 07:13:06 +0200

    manifest debug

--- project/_src/quick-view/manifest.php
+++ project/_src/quick-view/manifest.php
@@ -27,7 +27,7 @@ foreach ($iconsSizes as $size){
 "display": "standalone",
 "orientation": "portrait",
 "background_color": "#222222",
-"theme_color": "#",
+"theme_color": "#FF0000",
 "icons": <?=json_encode($icons,JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES)?>
 
 
