3246f58b9b42adda9707e3a5e5c1048819e83ba3
david <d.marsalone@gmail.com>
2019-06-01 11:04:04 +0200

    typography .fsz-1,.fsz-2,.fsz-3,etc pour des tailles de typo non responsive

--- project/_src/quick-view/style-guide/atomes/typography.php
+++ project/_src/quick-view/style-guide/atomes/typography.php
@@ -55,3 +55,35 @@
 
     </div>
 </div>
+
+<div class="container my-big">
+
+
+    <hr class="col-md-12 ghost-10">
+
+    <div class="row">
+
+        <div class="col-md-3">
+            <h4 class="">Corps de typo non responsive</h4>
+        </div>
+        <div class="col-md-9 mb-big">
+            <p>On utilise ces corps de typographie dans les organismes qui n'ont pas vocation à changer de taille en fonction de la taille d'écran.</p>
+        </div>
+
+
+        <?foreach (["fsz-1","fsz-2","fsz-3","fsz-4","fsz-5","fsz-6"] as $fs):?>
+
+            <div class="col-md-3">
+                <label>Style <?=$fs?></label>
+            </div>
+
+            <div class="col-md-9 mb-big">
+                <h1 class="<?=$fs?> fg-black">Sur un h1, <?=utils()->string->loremIspum(30,30)?></h1>
+                <h3 class="<?=$fs?> fg-fluo-2">Sur un h3, <?=utils()->string->loremIspum(30,30)?></h3>
+                <div class="<?=$fs?> fg-grey-dark">Sur un div, <?=utils()->string->loremIspum(30,30)?></div>
+            </div>
+
+        <?endforeach;?>
+
+    </div>
+</div>
