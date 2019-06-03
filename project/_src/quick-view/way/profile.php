83a8c83ca23e32206dbf4e7668486f8f0be804fe
david <d.marsalone@gmail.com>
2019-06-03 10:42:55 +0200

    tests css avec way

---
+++ project/_src/quick-view/way/profile.php
@@ -0,0 +1,182 @@
+<?php
+$view->inside("layout/layout", $vv);
+?>
+
+<div class="container text-center py-big">
+    <img alt="user name"
+         class="shp-circle sz-big"
+         src="https://le-drone.com/__cache/robert/img/__db/localhost/glspxueq_drone/pp/uids/84640/vignette_serre.jpg/150-150-nb/ffffff-80.jpg">
+        <h1 class="fg-fluo-2 h3 py-medium">Killian Bidet</h1>
+        <div class="fg-fluo-2">
+            <a href="#tag"> Etudiant</a> |
+            <a href="#tag"> Institut Catholique de Paris</a>
+            <br>
+            <a href="#tag"> Licence Humanité</a>
+        </div>
+        <div class="fg-fluo-2 py-medium">
+            <a href="#" class="button naked  sz-small negative-hvr color-instagram-hvr square">
+                <?=pov()->svg->use("startup-social-instagram")?>
+            </a>
+            <a href="#" class="button naked  sz-small negative-hvr color-facebook-hvr square">
+                <?=pov()->svg->use("startup-social-facebook")?>
+            </a>
+            <a href="#" class="button naked  sz-small negative-hvr color-twitter-hvr square">
+                <?=pov()->svg->use("startup-social-twitter")?>
+            </a>
+            <a href="#" class="button naked  sz-small negative-hvr color-linkedin-hvr square">
+                <?=pov()->svg->use("startup-social-linkedin")?>
+            </a>
+        </div>
+</div>
+
+
+<div class="card ratio-2x1 hvr">
+    <div class="bg fx-darken fx-level-25 fx-level-50-hvr">
+        <img src="https://le-drone.com/__cache/robert/img/__db/localhost/ledrgnpu_drone/pp/uids/92653/neon_cropped_ok.jpg/0-500-nb/FFFFFF-80.jpg">
+    </div>
+    <div class="fg">
+        <h1>What About Yours ?</h1>
+    </div>
+
+</div>
+
+<div class="container text-center py-big">
+    <div>
+        <h2 class="h1 fg-fluo-2 mb-big">Le premier réseau social d'orientation.</h2>
+        <p class="fg-fluo-2 mb-big"><?=utils()->string->loremIspum(50,50)?></p>
+
+        <div class="d-flex flex-column justify-content-center align-items-center">
+            <a class="button color-fluo-2 negative shadow-big mb-medium">Se connecter</a>
+            <a class="button color-fluo-2">S'inscrire</a>
+        </div>
+
+    </div>
+</div>
+
+<div class="bg-fluo-2 shp-rounded py-big text-center">
+    <div class="container">
+        <h2 class="fg-white mb-big">Toi aussi, donne ton avis !</h2>
+        <div class="row">
+            <?for ($i=0;$i<4;$i++):?>
+            <div class="col-md-6 col-xl-3 mb-medium">
+                <div class="card shp-rounded shadow-big ratio-4x3">
+                    <div class="bg fx-darken fx-level-25">
+                        <img src="https://interactive-examples.mdn.mozilla.net/media/examples/plumeria.jpg">
+                    </div>
+                    <div class="fg">
+                        <a class="button color-white transparent mb-small">
+                            #sciences
+                        </a>
+                        <a class="button color-fluo negative">
+                            Je donne mon avis
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <?endfor;?>
+        </div>
+
+
+    </div>
+</div>
+<div class="bg-fluo shp-rounded py-big text-center">
+    <div class="container">
+        <h2 class="fg-fluo-2 mb-big">A la une ...</h2>
+        <div class="cards-list shadow-big-list shp-rounded-list">
+        <?for ($i=0;$i<4;$i++):?>
+
+                <div>
+                    <div href="#" class="card hvr ratio-4x3">
+                        <div class="bg fx-gradient-bottom color-fluo-2 fx-level-75 fx-level-100-hvr">
+                            <img alt="test" src="https://le-drone.com/__cache/robert/img/__db/localhost/glspxueq_drone/dev/uids/57474/LIGHTNING_BOLT_PIX_1-1.jpg/380-214-nb/ffffff-80.jpg">
+                        </div>
+                        <div class="fg">
+                            <div class="top right">
+                                <a href="#tag-redaction" class="button square color-fluo-2 negative shp-rounded">
+                                    <?=pov()->svg->use("startup-social-instagram")?>
+                                </a>
+                            </div>
+                            <div class="bottom left right text-left pxy-small">
+                                <?=pov()->svg->use("startup-social-youtube")
+                                    ->addClass("h-medium w-medium fg-white")
+                                ?>
+                                <h3 class="fsz-4 text-shadow-flat">
+                                    Marie, nous parle de son entrée à la fac
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div href="#" class="card hvr ratio-4x3">
+                        <div class="bg fx-gradient-bottom color-fluo-2 fx-level-75  fx-level-100-hvr">
+                            <img alt="test" src="https://le-drone.com/__cache/robert/img/__db/localhost/glspxueq_dev/theDrone/uids/20265/helenah.jpg/0-500-nb/FFFFFF-80.jpg">
+                        </div>
+                        <div class="fg">
+                            <div class="top right">
+                                <a href="#tag-redaction" class="button square color-fluo-2 negative shp-rounded">
+                                    <?=pov()->svg->use("startup-social-instagram")?>
+                                </a>
+                            </div>
+                            <div class="bottom left right text-left pxy-small">
+                                <h3 class="fsz-4 text-shadow-flat">
+                                    Jayci : " Selon moi, les réseaux sociaux, il faut les utiliser comme un tremplin "
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div href="#" class="hvr card ratio-4x3">
+                        <div class="bg fx-gradient-bottom color-fluo-2 fx-level-75 fx-level-100-hvr">
+                            <img alt="test" src="https://le-drone.com/__cache/robert/img/__db/localhost/ledrgnpu_drone/pp/uids/92907/peacock_1.jpg/0-500-nb/FFFFFF-80.jpg">
+                        </div>
+                        <div class="fg">
+                            <div class="top right">
+                                <a href="#tag-redaction" class="button square color-fluo-2 negative shp-rounded">
+                                    <?=pov()->svg->use("startup-social-instagram")?>
+                                </a>
+                            </div>
+                            <a href="#" class="button transparent color-white negative-hvr">
+                                Intermittent du spectacle
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div href="#" class="hvr card ratio-4x3">
+                        <div class="bg fx-gradient-bottom color-fluo-2 fx-level-75 fx-level-100-hvr">
+                            <img alt="test" src="https://le-drone.com/__cache/robert/img/__db/localhost/ledrgnpu_drone/pp/uids/92907/peacock_1.jpg/0-500-nb/FFFFFF-80.jpg">
+                        </div>
+                        <div class="fg">
+                            <div class="top right">
+                                <a href="#tag-redaction" class="button square color-fluo-2 negative shp-rounded">
+                                    <?=pov()->svg->use("startup-social-instagram")?>
+                                </a>
+                            </div>
+                            <div class="bottom left right text-left pxy-small">
+                                <h4 class="fsz-4 mb-small text-shadow-flat">
+                                    Eloïse Chamalet
+                                </h4>
+                                <a href="#" class="button sz-small transparent color-white negative-hvr">
+                                    Master STAPS Ingénerie
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+        <?endfor;?>
+        </div>
+    </div>
+</div>
+
