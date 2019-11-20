c06f4f5901d3cd9dc9c264f1fe9a81c21e3b3fb2
david <d.marsalone@gmail.com>
2019-11-20 06:40:59 +0100

    exemple de customisation d'admin

---
+++ project/_src/cq-admin/cq-big-menu/config/links.after.php
@@ -0,0 +1,5 @@
+<label>Après la liste de liens</label>
+<a class="cq-list-item" href="<?=\Classiq\C_classiq::quickView_url("import/frais-de-port")?>" target="_blank">
+    <?=pov()->svg->use("cq-forward")->addClass("start")?>
+    world
+</a>
