ad1fb3dd22911b13044b959af0b8ee259c431dda
david <d.marsalone@gmail.com>
2019-11-20 06:40:15 +0100

    exemple de customisation de menu

---
+++ project/_src/cq-admin/cq-big-menu/config/links.before.php
@@ -0,0 +1,5 @@
+<label>Avant la liste de liens</label>
+<a class="cq-list-item" href="<?=\Classiq\C_classiq::quickView_url("import/frais-de-port")?>" target="_blank">
+    <?=pov()->svg->use("cq-forward")->addClass("start")?>
+    Hello
+</a>
