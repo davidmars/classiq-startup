51fe4b72b9d20f33f6eff7f8cf60bc8f0adadfa9
david <d.marsalone@gmail.com>
2019-11-20 06:42:06 +0100

    customisation de menu

--- project/_src/cq-admin/cq-big-menu/config/links.after.php
+++ project/_src/cq-admin/cq-big-menu/config/links.after.php
@@ -1,5 +1,5 @@
 <label>Après la liste de liens</label>
-<a class="cq-list-item" href="<?=\Classiq\C_classiq::quickView_url("import/frais-de-port")?>" target="_blank">
+<a class="cq-list-item" href="<?=\Classiq\C_classiq::quickView_url("world")?>" target="_blank">
     <?=pov()->svg->use("cq-forward")->addClass("start")?>
     world
 </a>
